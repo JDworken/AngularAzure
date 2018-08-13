3676510bb29ceb1b3984a85b0c073eb760d760ea
Jason Dworken <jasonsd90@yahoo.com>
2018-08-12 22:31:05 -0400

    - adding some module

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ProfileModule } from "./profile/profile.module";
+import { AppRoutingModule } from './app-routing.module';
+import { RouteComponent } from "./route.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProfileModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
