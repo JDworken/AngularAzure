737cf36dbb8b3b14d426153b4bab6edcdba598d9
Jason Dworken <jasonsd90@yahoo.com>
2018-08-20 21:36:46 -0400

    - stylin

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatIconRegistry } from "@angular/material";
 
 import { AppComponent } from './app.component';
 import { ProfileModule } from "./profile/profile.module";
 import { AppRoutingModule } from './app-routing.module';
 import { RouteComponent } from "./route.component";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,10 +15,16 @@ import { RouteComponent } from "./route.component";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ProfileModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    MatIconRegistry
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
